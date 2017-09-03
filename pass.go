8c53a5ecb0075a6b3d89e17002b59125fb33f7da
Hilmar Wiegand <hilmar.wiegand@flameshome.net>
2017-09-03 04:13:29 +0200

    :tada: Initial commit

---
+++ pass.go
@@ -0,0 +1,84 @@
+// Package xkcdpass provides functions for generating random passwords consisting of
+// words taken from a dictionary.
+// The wordlist is provided by the EFF (https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases).
+// Inspired by https://xkcd.com/936/
+package xkcdpass
+
+import (
+	"bytes"
+	"crypto/rand"
+	"math/big"
+	"strings"
+
+	"github.com/nbutton23/zxcvbn-go"
+)
+
+var words []string
+var max *big.Int
+
+const defaultLength = 4
+const defaultStrength = 4
+
+func init() {
+	data, err := Asset("static/default")
+	if err != nil {
+		panic("Failed to read word list: " + err.Error())
+	}
+
+	dict := string(data)
+	words = strings.Split(dict, "\n")
+	for index, w := range words {
+		words[index] = strings.Title(strings.TrimSpace(w))
+	}
+
+	max = big.NewInt(int64(len(words)) - 1)
+}
+
+// GenerateDefault generates a random 4-word password.
+func GenerateDefault() string {
+	return makePassword(defaultLength)
+}
+
+// GenerateWithLength generates a random password with the specified number of words.
+func GenerateWithLength(numWords int) string {
+	return makePassword(numWords)
+}
+
+// GenerateDefaultChecked generates a random 4-word password with a zxcvbn strength score of 4.
+func GenerateDefaultChecked() string {
+	return makePasswordWithStrength(defaultLength, defaultStrength)
+}
+
+// GenerateWithLengthAndStrength generates a random password with the specified number of words and a score equal to or higher to the specified strength.
+func GenerateWithLengthAndStrength(length int, strength int) string {
+	return makePasswordWithStrength(length, strength)
+}
+
+func makePassword(numWords int) string {
+	var b bytes.Buffer
+	for i := 0; i < numWords; i++ {
+		n, err := rand.Int(rand.Reader, max)
+		index := int(n.Int64())
+
+		if err != nil {
+			panic("Failed to generate random number: " + err.Error())
+		}
+
+		w := strings.TrimSpace(words[index])
+		b.WriteString(strings.Title(w))
+	}
+
+	return b.String()
+}
+
+func makePasswordWithStrength(numWords int, strength int) string {
+	currenctStrength := 0
+	var result string
+	for currenctStrength < strength {
+		result = makePassword(numWords)
+		entropy := zxcvbn.PasswordStrength(result, nil)
+		currenctStrength = entropy.Score
+	}
+
+	return result
+}
---
+++ pass_test.go
@@ -0,0 +1,36 @@
+package xkcdpass
+
+import (
+	"fmt"
+	"testing"
+	"unicode"
+
+	"github.com/nbutton23/zxcvbn-go"
+)
+
+func TestGenerateDefault(t *testing.T) {
+	pass := GenerateDefault()
+	fmt.Printf("generated password %v\n", pass)
+
+	numUppercase := 0
+	for _, c := range pass {
+		if unicode.IsUpper(c) {
+			numUppercase++
+		}
+	}
+
+	if numUppercase != defaultLength {
+		t.Error("Number of words was not 4")
+	}
+}
+
+func TestGenerateDefaultChecked(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		pass := GenerateDefaultChecked(i)
+		fmt.Printf("generated password %v\n", pass)
+		entropy := zxcvbn.PasswordStrength(pass, nil)
+		if entropy.Score < i {
+			t.Errorf("Failed to generate password with a score of at least %v", i)
+		}
+	}
+}
